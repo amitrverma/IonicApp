6c7fdfc7db3a21b740cca68d6ad71a02399e5a27
Amit Verma <P10267680@capita.co.in>
2016-05-20 12:59:34 +0530

    added controller

--- www/js/app.js
+++ www/js/app.js
@@ -5,6 +5,15 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('todo', ['ionic'])
 
+.controller("TodoCtrl", function($scope) {
+    $scope.tasks = [
+      { title: 'Collect coins' },
+      { title: 'Eat mushrooms' },
+      { title: 'Get high enough to grab the flag' },
+      { title: 'Find the Princess' }
+    ];
+});
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
